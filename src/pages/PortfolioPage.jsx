b04d42665d7e9ccb6bd5bd955bcde50459c2966d
CenkBerkay <36186960+CenkBerkay@users.noreply.github.com>
2024-02-01 12:39:17 +0100

    Update PortfolioPage.jsx

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -1,12 +1,15 @@
 import { PortfolioItemCard } from "../components/PortfolioItemCard";
-import { data } from "../utils/data";
 import { Wrap } from "@chakra-ui/react";
 
-export const PortfolioPage = ({ clickFn }) => {
+export const PortfolioPage = ({ items, clickFn }) => {
   return (
     <Wrap justify="center" spacing={38} py={10} bgColor="blue.100">
-      {data.hits.map((item) => (
-        <PortfolioItemCard clickFn={clickFn} item={item.recipe} key={item} />
+      {items.map((item) => (
+        <PortfolioItemCard
+          clickFn={clickFn}
+          item={item}
+          key={item.recipe.label}
+        />
       ))}
     </Wrap>
   );
