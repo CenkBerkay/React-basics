fd3e172a0dacfbf388c4f5784f5cf8aedc941778
CenkBerkay <36186960+CenkBerkay@users.noreply.github.com>
2024-02-06 11:45:53 +0100

    Update TextInput.jsx

--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -1,5 +1,10 @@
 import { Input } from "@chakra-ui/react";
 
 export const TextInput = ({ changeFn, ...props }) => (
-  <Input variant="flushed" onChange={changeFn} {...props} />
+  <Input
+    placeholder="Search Recipes"
+    bg={"white"}
+    onChange={changeFn}
+    {...props}
+  />
 );
