bb76871199a99cfccf3a47a484e33dd4ba629dab
CenkBerkay <36186960+CenkBerkay@users.noreply.github.com>
2024-02-06 11:44:54 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 import { PortfolioItemPage } from "./pages/PortfolioItemPage";
-import { PortfolioPage } from "./pages/PortfolioPage";
-//import { SearchFunction } from "./components/SearchFunction";
+import { data } from "./utils/data";
+import { SearchFunction } from "./components/SearchFunction";
+import { Wrap } from "@chakra-ui/react";
 
 export const App = () => {
   const [selectedItem, setSelectedItem] = useState();
 
   return (
-    <div>
+    <Wrap justify="center" py={10} bgColor="blue.100">
       {selectedItem ? (
         <PortfolioItemPage item={selectedItem} clickFn={setSelectedItem} />
       ) : (
-        <PortfolioPage clickFn={setSelectedItem} />
+        <SearchFunction items={data.hits} clickFn={setSelectedItem} />
       )}
-    </div>
+    </Wrap>
   );
 };
+
+
